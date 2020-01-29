c7875026bbab2c46b719935ccb3adc58d8bb7d0d
galitsky <eugen.galitsky@spryker.com>
2020-01-29 10:47:29 +0200

    Homework 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
 
 import { LoggerService } from './logger.service';
+import { HelloComponent } from './hello.component';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+
+export class AppComponent implements AfterViewInit {
+  @ViewChild(HelloComponent, {static: false}) childReference;
   name = 'Angular';
 
   stateLog: string[] = [];
 
   constructor(private loggerService: LoggerService) {}
 
-  onStateChanged(state: boolean) {
-    this.stateLog.push(`State became ${state}`);
-    this.loggerService.log(`AppComponent: New state: ${state}`);
+  ngAfterViewInit() {
+      this.childReference.stateChange.subscribe((state: boolean) => {
+          this.stateLog.push(`State became ${state}`);
+          this.loggerService.log(`AppComponent: New state: ${state}`);
+      });
   }
 }
